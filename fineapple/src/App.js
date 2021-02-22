bde9889907ca23b0c5416ea0c4744429ad73eb35
emily <emiliyoun@gmail.com>
2021-02-22 16:08:26 +0900

    [update] image location

--- fineapple/src/App.js
+++ fineapple/src/App.js
@@ -1,10 +1,22 @@
+import React ,{Component} from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+import Login from './Components/Login/Login'
+
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+              <Header />
+      <Switch>
+        <Route path='/' exact component={Main} /> 
+        <Route path='/Login' component={Login} />
+        {/*<Route path='/signin' component={Auth} /> 
+        <Route path="/feed" component={feed} /> */}
+      </Switch>
+    </Router>
   );
 }
 
