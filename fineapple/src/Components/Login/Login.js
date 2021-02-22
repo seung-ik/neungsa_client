a8e4c413badeb52b5f423a06e3e534ef5a2c8a13
emily <emiliyoun@gmail.com>
2021-02-22 16:05:55 +0900

    [feat] update page skeleton

---
+++ fineapple/src/Components/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Login.css'
+
+function Login() {
+    return (
+        <h1>Login</h1>
+    )
+}
+
+export default Login
