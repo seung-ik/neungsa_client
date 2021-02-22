ae1bd375c87378377ee1424c487d48694935991b
emily <emiliyoun@gmail.com>
2021-02-23 01:26:06 +0900

    [feat] create main page skeleton and css

---
+++ fineapple/src/Components/Main/Inspration.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Inspration.css'
+
+function Inspration() {
+    return (
+        <h1>Inspration</h1>
+    )
+}
+
+export default Inspration
---
+++ fineapple/src/Components/Main/Reviews.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Reviews.css'
+
+function Reviews() {
+    return (
+        <h1>Reviews</h1>
+    )
+}
+
+export default Reviews
