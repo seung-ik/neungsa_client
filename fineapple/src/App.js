5c5a2052f8d411eb3d976220569c81248b924e77
emily <emiliyoun@gmail.com>
2021-03-10 15:02:04 +0900

    [feat] update team page

--- fineapple/src/App.js
+++ fineapple/src/App.js
@@ -10,6 +10,7 @@ import Write from "./Components/Write/Write";
 import Mypage from "./Components/Mypage/Mypage";
 import FeedAndPost from "./Components/FeedandPost/FeedandPost";
 import MypageUpdate from "./Components/Write/Components/PageUpdateMy";
+import Team from './Components/Others/TeamPage/TheTeam'
 // import WorkTitle from './Components/Write/Components/WorkTitle'
 function App() {
   const [onChat, setOnChat] = useState(false);
@@ -30,7 +31,7 @@ function App() {
         <Route path="/write" component={Write} />
         <Route path="/mypage/update" component={MypageUpdate} />
         <Route path="/mypage" exact component={Mypage} />
-
+        <Route path="/theteam" exact component={Team} />
         {/* <Route path="/work/title" component={WorkTitle} />
         <Route path="/work/description" render={WorkDes} />
         <Route path="/work/budget" render={WorkBudget} />
