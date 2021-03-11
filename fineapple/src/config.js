64ba1f7d90f8800705506393d6e74cd495bb3c37
emilyoun <71864992+emilyoun@users.noreply.github.com>
2021-03-11 10:21:26 +0900

    Delete config.js

--- fineapple/src/config.js
+++
@@ -1,5 +0,0 @@
-export const bucketName = "fineapple-images-buket";
-export const dirName = "photos";
-export const region = "us-east-1";
-export const accessKeyId = "AKIA6BRXSFIXS6FK72HX";
-export const secretAccessKey = "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";
